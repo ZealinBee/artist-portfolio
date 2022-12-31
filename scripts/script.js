f3cea67addd33f3c1abe6d466c3c6348011131db
Zhiyuan Liu <thenukezealot@gmail.com>
2022-12-31 20:28:04 +0200

    added assets

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
 const spinner = document.querySelector('.spinner-wrapper')
 const mainText = document.querySelector('.main-text')
+const topText = document.querySelector('.top-text')
+const myTime = document.querySelector('.my-time')
 
 window.addEventListener('load', function() {
     spinner.classList.add('hidden')
@@ -7,4 +9,12 @@ window.addEventListener('load', function() {
         document.body.removeChild(spinner)
     })
     mainText.classList.add('transform-back-x')
+    topText.classList.add('transform-back-x')
+    // Gets the current time in the Philippines
+    var d = new Date() 
+    var utc = d.getTime() + (d.getTimezoneOffset() * 60000)
+    var nd = new Date(utc + (3600000*8))
+    var currentDatePhilippines = nd.toLocaleString()
+    myTime.textContent = currentDatePhilippines
 })
+
