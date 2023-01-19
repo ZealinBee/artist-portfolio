6be95739ef2a7aae5ef0f26c27bfec716b27f249
Zhiyuan Liu <thenukezealot@gmail.com>
2023-01-19 10:13:56 +0200

    scrollbar, clipboard

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,28 @@
-const spinner = document.querySelector('.spinner-wrapper')
-const mainText = document.querySelector('.main-text')
-const topText = document.querySelector('.top-text')
-const descriptionText = document.querySelector('.description-text-wrapper')
-const myTime = document.querySelector('.my-time')
+const spinner = document.querySelector(".spinner-wrapper");
+const mainText = document.querySelector(".main-text");
+const topText = document.querySelector(".top-text");
+const descriptionText = document.querySelector(".description-text-wrapper");
+const myTime = document.querySelector(".my-time");
+const clipboard = new ClipboardJS(".email-copy");
+const clipboardWrapper = document.querySelector('.email-copy')
 
-window.addEventListener('load', function() {
-    spinner.classList.add('hidden')
-    spinner.addEventListener('transitionend', function() {
-        document.body.removeChild(spinner)
-    })
-    mainText.classList.add('transform-back-x')
-    topText.classList.add('transform-back-x')
-    descriptionText.classList.add('transform-back-x')
-    // Gets the current time in the Philippines
-    var d = new Date() 
-    var utc = d.getTime() + (d.getTimezoneOffset() * 60000)
-    var nd = new Date(utc + (3600000*8))
-    var currentDatePhilippines = nd.toLocaleString()
-    myTime.textContent = currentDatePhilippines
+window.addEventListener("load", function () {
+  spinner.classList.add("hidden");
+  spinner.addEventListener("transitionend", function () {
+    document.body.removeChild(spinner);
+  });
+  mainText.classList.add("transform-back-x");
+  topText.classList.add("transform-back-x");
+  descriptionText.classList.add("transform-back-x");
+  // Gets the current time in the Philippines
+  var d = new Date();
+  var utc = d.getTime() + d.getTimezoneOffset() * 60000;
+  var nd = new Date(utc + 3600000 * 8);
+  var currentDatePhilippines = nd.toLocaleString();
+  myTime.textContent = currentDatePhilippines;
+});
+
+clipboardWrapper.addEventListener('click', function() {
+    clipboardWrapper.classList.add('active')
 })
 
