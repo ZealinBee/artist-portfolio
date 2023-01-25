6153c1bf7eaea6e00c2a106375fe3f32e0d00bd7
Zhiyuan Liu <thenukezealot@gmail.com>
2023-01-25 10:39:29 +0200

    filtering system implemented

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,7 @@ const topText = document.querySelector(".top-text");
 const descriptionText = document.querySelector(".description-text-wrapper");
 const myTime = document.querySelector(".my-time");
 const clipboard = new ClipboardJS(".email-copy");
-const clipboardWrapper = document.querySelector('.email-copy')
+const clipboardWrapper = document.querySelector(".email-copy");
 
 window.addEventListener("load", function () {
   spinner.classList.add("hidden");
@@ -22,7 +22,49 @@ window.addEventListener("load", function () {
   myTime.textContent = currentDatePhilippines;
 });
 
-clipboardWrapper.addEventListener('click', function() {
-    clipboardWrapper.classList.add('active')
+clipboardWrapper.addEventListener("click", function () {
+  clipboardWrapper.classList.add("active");
+  setTimeout(function () {
+    clipboardWrapper.classList.remove("active");
+  }, 2500);
+});
+
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset > 100) {
+    document.querySelector(".pop-up-1").style.transform =
+      "translateX(0%)"; /* on-screen position */
+    document.querySelector(".pop-up-1").style.opacity =
+      "1"; /* on-screen position */
+  }
+});
+
+const artSections = document.querySelectorAll(".art-section");
+const allSectionButton = document.querySelector(".all-button");
+allSectionButton.classList.add('selected-button');
+filterSection("all");
+
+
+function filterSection(sectionCategory) {
+  artSections.forEach((artSection) => {
+    if (
+      sectionCategory === "all" ||
+      artSection.getAttribute("data-category") === sectionCategory
+
+    ) {
+      artSection.style.display = "grid";
+    } else {
+      artSection.style.display = "none";
+    }
+  });
+}
+
+const filterButtons = document.querySelectorAll('.filter-button')
+
+filterButtons.forEach(filterButton => {
+  filterButton.addEventListener('click', e => {
+    filterButtons.forEach(filterBtn => filterBtn.classList.remove('selected-button'))
+    filterButton.classList.add('selected-button')
+    filterSection(e.target.dataset.category)
+  })
 })
 
